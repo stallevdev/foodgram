c43972f04d9edba43f34e28557b22ece79dc0905
Sergey Tsepilov <stallevdev@gmail.com>
2024-10-15 19:24:25 +0500

    urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,10 +3,15 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from .views import CustomUserViewSet
+
 app_name = 'api'
 
 router = routers.DefaultRouter()
+router.register('users', CustomUserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
