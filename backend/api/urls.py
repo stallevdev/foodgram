0d86c9dcacefe6208798d8ecbf9be0b95776540c
Sergey Tsepilov <stallevdev@gmail.com>
2024-10-15 17:22:25 +0500

    urls

---
+++ backend/api/urls.py
@@ -0,0 +1,12 @@
+"""Маршруты для API-приложения."""
+
+from django.urls import include, path
+from rest_framework import routers
+
+app_name = 'api'
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
