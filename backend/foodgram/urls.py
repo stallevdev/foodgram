1c1e921bff3ab6a1c7d4b24c83e6118caaefd4c2
Sergey Tsepilov <stallevdev@gmail.com>
2024-10-15 17:21:18 +0500

    urls

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,8 +1,9 @@
 """Настройка административной панели Django."""
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
