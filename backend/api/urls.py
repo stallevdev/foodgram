552f2b0ab0e9b222b8e60f6dfcca83974db70b5e
Sergey Tsepilov <stallevdev@gmail.com>
2024-10-15 17:36:05 +0500

    urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,12 +3,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import CustomUserViewSet
+from .views import CustomUserViewSet, TagViewSet
 
 app_name = 'api'
 
 router = routers.DefaultRouter()
 router.register('users', CustomUserViewSet, basename='user')
+router.register('tags', TagViewSet, basename='tag')
 
 urlpatterns = [
     path('', include(router.urls)),
